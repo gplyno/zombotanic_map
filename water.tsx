4ccf089b3968e538c482fe15b377a78f32737b2f
gplyno <gplyno@gmail.com>
2026-08-12 14:30:44 +0500

    Add files via upload

---
+++ water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="water" tilewidth="32" tileheight="32" tilecount="529" columns="23">
+ <image source="water.jpg" width="736" height="736"/>
+</tileset>
