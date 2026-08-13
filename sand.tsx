2a11276349f31ddc442217afaaa74ef8e339bccb
gplyno <gplyno@gmail.com>
2026-08-13 17:01:39 +0500

    Delete sand.tsx

--- sand.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="sand" tilewidth="32" tileheight="32" tilecount="330" columns="22">
- <image source="sand.jpg" width="735" height="488"/>
-</tileset>
