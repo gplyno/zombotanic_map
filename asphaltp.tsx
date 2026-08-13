2e8775797b520bd456dbcdbeda0cbc8b92fd7ae4
gplyno <gplyno@gmail.com>
2026-08-13 17:00:27 +0500

    Delete asphaltp.tsx

--- asphaltp.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="asphalt" tilewidth="32" tileheight="32" tilecount="342" columns="19">
- <image source="asphalt.png" width="639" height="596"/>
-</tileset>
